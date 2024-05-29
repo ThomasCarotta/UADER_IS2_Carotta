7fef3a0d1ded19fb99efabb50f278a8c448c7179
thomas carotta <thomasgabrielcarotta@outlook.com>
2024-05-29 20:14:46 -0300

    TP6 IS2

--- TP6_IS2/getJason.py
+++ TP6_IS2/getJason.py
@@ -1,208 +1,56 @@
-# # uncompyle6 version 3.9.1
-# # Python bytecode version base 3.6 (3379)
-# # Decompiled from: Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)]
-# # Embedded file name: getJason.py
-# # Compiled at: 2022-06-14 16:15:55
-# # Size of source mod 2**32: 214 bytes
-
-# import json, sys
-# jsonfile = sys.argv[1]
-# jsonkey = sys.argv[2]
-# with open(jsonfile, "r") as myfile:
-#     data = myfile.read()
-# obj = json.loads(data)
-# print(str(obj[jsonkey]))
-
-#------------------Vercion ejercicio 3------------------
-# #copyright UADER FCyT-IS2©2024 todos los derechos reservados
-
-# import json
-# import sys
-
-# class SingletonMeta(type):
-#     _instances = {}
-
-#     def __call__(cls, *args, **kwargs):
-#         if cls not in cls._instances:
-#             instance = super().__call__(*args, **kwargs)
-#             cls._instances[cls] = instance
-#         return cls._instances[cls]
-
-# class TokenExtractor(metaclass=SingletonMeta):
-#     version = "[1.1]"
-
-#     def __init__(self, jsonfile="", jsonkey="token1"):
-#         """
-#         Inicializa el extractor de token con el archivo JSON y la clave especificados.
-#         """
-#         self.jsonfile = jsonfile
-#         self.jsonkey = jsonkey
-
-#     def print_help(self):
-#         """
-#         Imprime el mensaje de ayuda del programa.
-#         """
-#         help_message = """
-# Extractor de token para acceso API Servicios Banco XXX (versión 1.1)
-
-# Este programa permite extraer la clave de acceso API para utilizar los servicios del 
-# Banco XXX.
-
-# El programa operará como un microservicio invocado mediante:s
-
-#     {path ejecutable}/getJason.py {path archivo JSON}/{nombre archivo JSON}.json [clave]
-
-# ej.
-#     ./getJason.py ./sitedata.json token1
-
-# El token podrá recuperarse mediante el standard output de ejecución en el formato
-
-#     {1.1}XXXX-XXXX-XXXX-XXXX
-
-# Para obtener un mensaje de ayuda detallado ejecutar
-
-#     ./getJason.py -h
-
-# Excepciones
-
-# Todas las condiciones de error del programa deben producir un mensaje de error bajo su control antes de
-# terminar.
-# """
-#         print(help_message)
-
-#     def print_version(self):
-#         """
-#         Imprime la versión del programa.
-#         """
-#         print(f"Versión {self.version}")
-
-#     def extract_token(self):
-#         """
-#         Extrae el token del archivo JSON utilizando la clave especificada.
-#         """
-#         try:
-#             with open(self.jsonfile, "r") as myfile:
-#                 data = myfile.read()
-#             obj = json.loads(data)
-#             token = str(obj[self.jsonkey])
-#             print(f"{self.version}{token}")
-#         except FileNotFoundError:
-#             print(f"Error: El archivo '{self.jsonfile}' no se encontró.")
-#         except KeyError:
-#             print(f"Error: La clave '{self.jsonkey}' no se encontró en el archivo JSON.")
-#         except json.JSONDecodeError:
-#             print(f"Error: El archivo '{self.jsonfile}' no contiene un JSON válido.")
-#         except Exception as e:
-#             print(f"Error: {str(e)}")
-
-# def main():
-#     if len(sys.argv) < 2 or sys.argv[1] == '-h':
-#         extractor = TokenExtractor()
-#         extractor.print_help()
-#         return
-    
-#     if sys.argv[1] == '-v':
-#         extractor = TokenExtractor()
-#         extractor.print_version()
-#         return
-
-#     if len(sys.argv) < 3:
-#         print("Error: Se esperaba al menos un argumento adicional.")
-#         return
-
-#     jsonfile = sys.argv[1]
-#     jsonkey = sys.argv[2]
-
-#     extractor = TokenExtractor(jsonfile, jsonkey)
-#     extractor.extract_token()
-
-# if __name__ == "__main__":
-#     main()
-
-#------------------Vercion ejercicio 4------------------
-# #copyright UADER FCyT-IS2©2024 todos los derechos reservados
-
 import json
 import sys
 
-class SingletonMeta(type):
-    _instances = {}
+class JSONReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-class TokenExtractor(metaclass=SingletonMeta):
-    version = "1.2"
-
-    def __init__(self, jsonfile="", jsonkey="token1"):
-        self.jsonfile = jsonfile
-        self.jsonkey = jsonkey
-
-    def print_help(self):
-        help_message = """
-Extractor de token para acceso API Servicios Banco XXX (versión 1.2)
-
-Este programa permite extraer la clave de acceso API para utilizar los servicios del 
-Banco XXX.
-
-El programa operará como un microservicio invocado mediante:
-
-    {path ejecutable}/getJason.py {path archivo JSON}/{nombre archivo JSON}.json [clave]
-
-ej.
-    ./getJason.py ./sitedata.json token1
-
-El token podrá recuperarse mediante el standard output de ejecución en el formato
-
-    {1.2}XXXX-XXXX-XXXX-XXXX
-
-Para obtener un mensaje de ayuda detallado ejecutar
-
-    ./getJason.py -h
-
-Excepciones
-
-Todas las condiciones de error del programa deben producir un mensaje de error bajo su control antes de
-terminar.
-"""
-        print(help_message)
-
-    def extract_token(self):
-        if len(sys.argv) < 2 or sys.argv[1] == '-h':
-            self.print_help()
-            return
-        
-        if sys.argv[1] == '-v':
-            print(f"Versión del programa: {self.version}")
-            return
-
-        if len(sys.argv) < 3:
-            print("Error: Se esperaba al menos un argumento adicional.")
-            return
-
-        jsonfile = sys.argv[1]
-        jsonkey = sys.argv[2]
-
+    def read_json(self):
         try:
-            with open(jsonfile, "r") as myfile:
+            with open(self.file_path, "r") as myfile:
                 data = myfile.read()
-            obj = json.loads(data)
-            banks = obj.get("bancos", {})
-            token = banks.get(jsonkey, {}).get("token", "")
-            if token:
-                print(f"{self.version}{token}")
-            else:
-                print(f"Error: La clave '{jsonkey}' no se encontró en el archivo JSON.")
+            return json.loads(data)
         except FileNotFoundError:
-            print(f"Error: El archivo '{jsonfile}' no se encontró.")
+            raise FileNotFoundError(f"Error: No se encontro el archivo {self.file_path} .")
         except json.JSONDecodeError:
-            print(f"Error: El archivo '{jsonfile}' no contiene un JSON válido.")
+            raise ValueError("Error: El archivo no es un JSON válido.")
+
+class TokenFormatter:
+    def format_token(token):
+        return f"{1.0}{token}"
+class JSONParser:
+    def parse_json(json_obj, key):
+        try:
+            return json_obj[key]
+        except KeyError:
+            raise KeyError(f"Error: La clave no se encontró en el archivo JSON: {key}.")
+
+class Program:
+    def __init__(self, json_reader, token_formatter, json_parser):
+        self.json_reader = json_reader
+        self.token_formatter = token_formatter
+        self.json_parser = json_parser
+
+    def run(self, json_key):
+        try:
+            json_obj = self.json_reader.read_json()
+            value = self.json_parser.parse_json(json_obj, json_key)
+            formatted_token = self.token_formatter.format_token(str(value))
+            print(formatted_token)
         except Exception as e:
-            print(f"Error: {str(e)}")
+            print(f"Error inesperado: {e}")
+            sys.exit(1)
 
 if __name__ == "__main__":
-    extractor = TokenExtractor()
-    extractor.extract_token()
+    if len(sys.argv) != 3 or sys.argv[1] == '-h':
+        print("Forma de usar el comando: {path ejecutable}/getJason.pyc {path archivo JSON}/{nombre archivo JSON}.json {clave}")
+        sys.exit(1)
+
+    json_file = sys.argv[1]
+    jsonkey = sys.argv[2]
+
+    json_reader = JSONReader(json_file)
+    token_formatter = TokenFormatter()
+    json_parser = JSONParser()
+    program = Program(json_reader, token_formatter, json_parser)
+    program.run(jsonkey)
